8f19236e6c11cb25f9d78cb00ea7b41e0922cd24
Minh Pham <minhpham1811@gmail.com>
2017-06-14 21:27:11 -0700

    Update setup file

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='SemanticLabeling',
     version='1.0',
-    packages=['semantic_labeling', "semantic_labeling.data_source", "semantic_labeling.machine_learning",
-              "semantic_labeling.main", "semantic_labeling.search_engine", "semantic_labeling.utils"],
+    packages=['semantic_labeling', "semantic_labeling.lib", "semantic_labeling.search",
+              "semantic_labeling.main", "semantic_labeling.tests"],
     url='',
     license='',
     author='Minh Pham',
